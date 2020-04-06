4e9636dc8d30679255676fd2a7f7757cd534c972
Khushboo Verma <khush.vv@gmail.com>
2020-04-07 00:21:10 +0530

    Connected to MySQL, added Add, Delete and Edit features

--- app.js
+++ app.js
@@ -1,25 +1,77 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
-app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
 
-// app.get('/', (req, res)=>{
-//     res.render('hello.ejs');
-// })
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'username',
+  password: 'password',
+  database: 'list_app'
+});
 
-// app.get('/top', (req,res)=>{
-//     res.render('top.ejs');
-// })
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
-app.get('/', (req, res)=>{
-    res.render('top.ejs');
-})
+app.get('/', (req, res) => {
+  res.render('top.ejs');
+});
 
-app.get('/index', (req, res)=>{
-    res.render('index.ejs');
-})
+app.get('/index', (req, res) => {
+  connection.query(
+    'SELECT * FROM items',
+    (error, results) => {
+      res.render('index.ejs', {items: results});
+    }
+  );
+});
+
+app.get('/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+app.post('/create', (req, res) => {
+  connection.query(
+    'INSERT INTO items (name) VALUES (?)',
+    [req.body.itemName],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
+
+app.post('/delete/:id', (req, res) => {
+  connection.query(
+    'DELETE FROM items WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
+
+app.get('/edit/:id', (req, res) => {
+  connection.query(
+    'SELECT * FROM items WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      res.render('edit.ejs', {item: results[0]});
+    }
+  );
+});
+
+app.post('/update/:id', (req, res) => {
+  connection.query(
+    'UPDATE items SET name = ? WHERE id = ?',
+    [req.body.itemName, req.params.id],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
 
 app.listen(3000);
-console.log('Server started on port 3000');
